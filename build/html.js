6f7b50ad567e80f7438d4a9a77ab3e89f776922d
Roy Hewitt <roy_hewitt@fws.gov>
2017-05-27 17:36:36 -0400

    Inject list items based on JSON file of locations

--- build/html.js
+++ build/html.js
@@ -1,11 +1,38 @@
 const fs = require('fs');
 const moment = require('moment');
+const cheerio = require('cheerio');
+const pretty = require('pretty');
 
+const data = require('../src/data/locations.json');
 const input = 'src/index.html';
 const output = 'dist/index.html';
-const DATE_PLACEHOLDER = /REPLACE_WITH_DATE/g;
+
 const now = moment().format('MMMM Do, YYYY');
 
-const html = fs.readFileSync(input, 'utf8');
-const finalHtml = html.replace(DATE_PLACEHOLDER, now);
-fs.writeFileSync(output, finalHtml, 'utf8');
+fs.readFile(input, 'utf8', (err, html) => {
+  if (err) console.error(err);
+  const $ = cheerio.load(html)
+  const listItems = data.features
+    .map(createListItem)
+    .join('');
+
+  $('.slides').append(listItems);
+  $('.last-updated').append(`Last updated: ${now}`);
+
+  writeFile( pretty($.html()) );
+});
+
+function createListItem(feature) {
+  return `
+    <li class="slide" aria-hidden="true">
+      <h2>${feature.properties.label}</h2>
+      <p>${feature.properties.text}</p>
+    </li>
+  `;
+}
+
+function writeFile(html) {
+  fs.writeFile(output, html, 'utf8', (err) => {
+    if (err) console.error(err);
+  });
+}
