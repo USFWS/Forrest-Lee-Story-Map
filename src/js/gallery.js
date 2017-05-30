6c629d71fdaff85946f5579d29002881edf9e3f9
Roy Hewitt <roy_hewitt@fws.gov>
2017-05-30 09:40:56 -0400

    Create gallery constructor function to handle images

---
+++ src/js/gallery.js
@@ -0,0 +1,12 @@
+const Gallery = function(opts) {
+  this.container = opts.container;
+  // this.items = Array.from(container.querySelectorAll('li'));
+  // this.container.addEventListener('mouseenter', toggleImage)
+  // this.container.addEventListener('mouseleave', toggleImage)
+}
+
+function toggleImage(e) {
+  const el = e.target;
+}
+
+module.exports = Gallery;
