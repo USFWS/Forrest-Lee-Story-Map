0588ad7b468ffea7d424126380339bab4b95f704
Roy Hewitt <roy_hewitt@fws.gov>
2017-06-13 13:22:41 -0400

    Rename slideshow => gallery

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,11 +1,11 @@
 const mediator = require('./mediator');
+const closest = require('closest');
 
-const Slideshow = function(opts) {
+const Gallery = function(opts) {
   // Get a list of slides to work width
   // Could add error handling that we're getting a UL/OL with child LI slides
   this.container = opts.slides;
   this.slides = Array.from(opts.slides.querySelectorAll('li.slide'));
-  const figures = Array.from(opts.slides.querySelectorAll('figure'));
 
   // Start on the correct slide (value from user: 1 = first slide, 2 = second, etc.)
   const isValidIndex = opts.currentSlide > 0 && opts.currentSlide < this.slides.length + 1;
@@ -19,11 +19,11 @@ const Slideshow = function(opts) {
   this.nextButton = opts.nextButton;
   this.previousButton.addEventListener('click', this.previous.bind(this));
   this.nextButton.addEventListener('click', this.next.bind(this));
-  this.container.addEventListener('click', figureEventHandler);
+  this.container.addEventListener('click', figureEventHandler.bind(this));
   document.body.addEventListener('keyup', keyHandler.bind(this));
 }
 
-Slideshow.prototype.next = function() {
+Gallery.prototype.next = function() {
   const index = this.currentSlide;
   if (index === this.slides.length - 1) {
     this.deactivateSlide(this.slides[index]);
@@ -38,7 +38,7 @@ Slideshow.prototype.next = function() {
   mediator.emit('slide', this.currentSlide);
 }
 
-Slideshow.prototype.previous = function() {
+Gallery.prototype.previous = function() {
   const index = this.currentSlide;
   if (index === 0) {
     this.deactivateSlide(this.slides[index]);
@@ -53,27 +53,27 @@ Slideshow.prototype.previous = function() {
   mediator.emit('slide', this.currentSlide);
 }
 
-Slideshow.prototype.goTo = function(index) {
+Gallery.prototype.goTo = function(index) {
   this.slides.forEach(s => this.deactivateSlide(s))
   this.activateSlide(this.slides[index]);
   mediator.emit('slide', index);
 }
 
-Slideshow.prototype.deactivateSlide = function(slide) {
+Gallery.prototype.deactivateSlide = function(slide) {
   slide.setAttribute('aria-hidden', 'true');
 }
 
-Slideshow.prototype.activateSlide = function(slide) {
+Gallery.prototype.activateSlide = function(slide) {
   slide.setAttribute('aria-hidden', 'false');
 }
 
 function figureEventHandler(e) {
-  const figure = e.target.parentNode;
+  const figure = closest(e.target, 'figure');
+  if (figure.nodeName !== 'FIGURE') return;
   const alt = e.target.alt;
   const src = e.target.src;
   const caption = figure.querySelector('figcaption').innerHTML;
-  if (figure.nodeName !== 'FIGURE') return;
-  mediator.emit('click:figure', {src, alt, caption})
+  mediator.emit('click:thumbnail', {src, alt, caption})
 }
 
 function keyHandler(e) {
@@ -82,4 +82,4 @@ function keyHandler(e) {
   if (key === 37) this.previous();
 }
 
-module.exports = Slideshow;
+module.exports = Gallery;
